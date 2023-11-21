efcb14b9bbe4f5249bbd55f08f48f6eb2f423062
Davi Samuel <davisamasoa@gmail.com>
2023-11-21 18:03:07 -0300

    fix(inteligencias): title spacing fixed

--- src/app/inteligencias/page.tsx
+++ src/app/inteligencias/page.tsx
@@ -38,9 +38,9 @@ const expertises: expertiseType[] = [
 export default function page({}: Props) {
 	return (
 		<div>
-			<h1 className="md:text-start text-center md:pl-[17.2%] lg:w-1/2 md:w-2/3 w-full px-9 sm:pt-10 sm:pb-10 pb-8 font-goudyOldStyle  md:text-4xl text-xl text-gray-800 opacity-90">
-				Queremos o perene, não o perecível. <br /> Buscamos elo com o que não se dilui como pó, mas ao que
-				resiste feito pedra.
+			<h1 className="md:text-start text-center md:pl-[17.2%]  w-full px-9 pb-10 font-goudyOldStyle  md:text-4xl text-xl text-gray-800 opacity-90">
+				Queremos o perene, não o perecível. <br /> Buscamos elo com o que não se dilui
+				<br /> como pó, mas ao que resiste feito pedra.
 			</h1>
 
 			{expertises.map((expertise, index) => (
